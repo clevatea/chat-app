c7f29005e85815588d7492f9cb3d23d95f5e0e56
khasin <khasin.madappe@gmail.com>
2019-12-28 01:10:04 +0700

    - add firebase env to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAvy4dAhXeRGOVHExoiTmXKuto9O4P3G9k',
+    authDomain: 'chat-app-6118d.firebaseapp.com',
+    databaseURL: 'https://chat-app-6118d.firebaseio.com',
+    projectId: 'chat-app-6118d',
+    storageBucket: 'chat-app-6118d.appspot.com',
+    messagingSenderId: '90080360320',
+    appId: '1:90080360320:web:4cf438e8e20a15bc74b365'
+  }
 };
